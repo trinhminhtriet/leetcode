e30d70e54115948bba999d96dceeb7a479148bb7
Trinh Minh Triet <contact@trinhminhtriet.com>
2025-08-13 10:16:29 +0700

    Update w2.sh to include git pull for leetcode-doocs repository and remove unnecessary commands

--- tools/bash/w2.sh
+++ tools/bash/w2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+
+cd /workspace/trinhminhtriet/leetcode-doocs
+echo ">>> Go to leetcode-doocs repository"
+git pull
+echo ">>> Updated leetcode-doocs repository"
+
 cd /workspace/trinhminhtriet/leetcode/tools
 
 python3 --version
-
 # python3 -m venv .venv
 . ./.venv/bin/activate
 
@@ -11,10 +16,8 @@ python3 --version
 # python -m pip install --upgrade -r requirements.txt
 
 python --version
-pip --version
 
 cd /workspace/trinhminhtriet/leetcode/tools/leetcode_solver
-
 python question.py
 echo ">>> Copied solutions from leetcode-doocs to leetcode"
 sleep 2
